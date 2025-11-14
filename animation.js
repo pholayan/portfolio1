35de46a2b16cded49f23cc0bd74fb3e0489593d1
pholayan <pholayan@gmail.com>
2025-11-14 13:56:40 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,16 @@
+// Trigger animation when elements enter viewport
+const animatedElements = document.querySelectorAll('.slide-up, .fade-in');
+
+function showAnimation() {
+    animatedElements.forEach(el => {
+        let position = el.getBoundingClientRect().top;
+        let screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 50) {
+            el.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", showAnimation);
+window.addEventListener("load", showAnimation);
